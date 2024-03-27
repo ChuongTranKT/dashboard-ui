bbe4100225f9d3abae0770f54565f28edbc4c139
ChuongTranKT <vanchuongktt1998@gmail.com>
2024-03-27 18:35:19 +0900

    setup: custom color and config prettierrc and eslintrc

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      purple: '#343C6A',
+      black: '#232323',
+      blue: '#1814F3',
+      light_blue: '#718EBF',
+      turquoise: '#16DBAA',
+      coral: '#FE5C73',
+      gray: '#B1B1B1',
+    },
+  },
+  plugins: [],
+}
+export default config
